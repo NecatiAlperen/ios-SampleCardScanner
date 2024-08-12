a8981d45085eacbb59450b1826825c68f3234f27
Necati Alperen Işık <necatialperen2@gmail.com>
2024-08-12 17:32:42 +0300

    first commit

---
+++ ios-SampleCardDetection/CameraManger.swift
@@ -0,0 +1,8 @@
+//
+//  CameraManger.swift
+//  ios-SampleCardDetection
+//
+//  Created by Necati Alperen IŞIK on 12.08.2024.
+//
+
+import Foundation
---
+++ ios-SampleCardDetection/ScannerManager.swift
@@ -0,0 +1,8 @@
+//
+//  ScannerManager.swift
+//  ios-SampleCardDetection
+//
+//  Created by Necati Alperen IŞIK on 11.08.2024.
+//
+
+import Foundation
